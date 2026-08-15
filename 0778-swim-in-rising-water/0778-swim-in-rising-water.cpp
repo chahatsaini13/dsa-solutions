8ce4d9ecd460f09674259dae20e53042917e0346
CHAHAT SAINI <chahatsaini1309@gmail.com>
2026-08-15 18:35:20 +0530

    Time: 4 ms (77.21%), Space: 12.7 MB (92.64%) - LeetHub

---
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    bool valid(int r, int c, int n) {
+        if(r < 0 || c < 0 || r >= n || c >= n) {
+            return false;
+        }
+
+        return true;
+    }
+
+    int swimInWater(vector<vector<int>>& grid) {
+
+        int n = grid.size();
+
+        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+
+        priority_queue<
+            pair<int, pair<int,int>>,
+            vector<pair<int, pair<int,int>>>,
+            greater<pair<int, pair<int,int>>>
+        > pq;
+
+        int x[4] = {1, -1, 0, 0};
+        int y[4] = {0, 0, 1, -1};
+
+        dist[0][0] = grid[0][0];
+
+        pq.push({grid[0][0], {0, 0}});
+
+        while(!pq.empty()) {
+
+            auto p = pq.top();
+            pq.pop();
+
+            int time = p.first;
+            int row = p.second.first;
+            int col = p.second.second;
+
+            if(time > dist[row][col]) {
+                continue;
+            }
+
+            if(row == n-1 && col == n-1) {
+                return time;
+            }
+
+            for(int k = 0; k < 4; k++) {
+
+                int r = row + x[k];
+                int c = col + y[k];
+
+                if(!valid(r, c, n)) {
+                    continue;
+                }
+
+                int newTime = max(time, grid[r][c]);
+
+                if(newTime < dist[r][c]) {
+
+                    dist[r][c] = newTime;
+
+                    pq.push({newTime, {r, c}});
+                }
+            }
+        }
+
+        return -1;
+    }
+};
