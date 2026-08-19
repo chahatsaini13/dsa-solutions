4aa2d56ef94c93991022fb8cb4b465fe2fb60e1e
CHAHAT SAINI <chahatsaini1309@gmail.com>
2026-08-19 23:23:01 +0530

    Time: 7 ms (28.49%), Space: 17.3 MB (90.19%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& edges, int src, int dst, int k) {
+        vector<int> res(n, 1e8);
+        res[src] = 0;
+        for(int i = 0; i <= k; i++){
+            vector<int> temp = res;
+            
+            for(int j = 0; j < edges.size(); j++){
+                int s = edges[j][0];
+                int d = edges[j][1];
+                int wt = edges[j][2];
+
+                if(res[s] != 1e8 && temp[d] > (res[s] + wt)){
+                    temp[d] = res[s] + wt;
+                }
+            }
+            res = temp;
+        }
+
+        if(res[dst] == 1e8){
+            return -1;
+        }
+
+        return res[dst];
+    }
+};
