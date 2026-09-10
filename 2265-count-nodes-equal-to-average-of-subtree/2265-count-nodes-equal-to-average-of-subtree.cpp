a2bcd5a611fb1f0d86ea4715beddd6fcc65f94fc
CHAHAT SAINI <chahatsaini1309@gmail.com>
2026-09-10 18:12:42 +0530

    Time: 7 ms (54.96%), Space: 15.8 MB (29.76%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -11,60 +11,30 @@
  */
 class Solution {
 public:
-    int calculateAverage(int sum, int count) {
-        return sum / count;
+    pair<int, int> solve(TreeNode* root, int& ans) {
+        if(root == nullptr) {
+            return {0, 0};
+        }
+
+        pair<int, int> left = solve(root->left, ans);
+        pair<int, int> right = solve(root->right, ans);
+
+        int sum = root->val + left.first + right.first;
+        int count = 1 + left.second + right.second;
+
+        int avg = sum / count;
+
+        if(avg == root->val) {
+            ans++;
+        }
+
+        return {sum, count};
     }
 
     int averageOfSubtree(TreeNode* root) {
         int ans = 0;
 
-        stack<TreeNode*> st;
-        stack<TreeNode*> order;
-
-        st.push(root);
-
-        while (!st.empty()) {
-            TreeNode* node = st.top();
-            st.pop();
-
-            order.push(node);
-
-            if (node->left != nullptr) {
-                st.push(node->left);
-            }
-
-            if (node->right != nullptr) {
-                st.push(node->right);
-            }
-        }
-
-        unordered_map<TreeNode*, pair<int, int>> data;
-
-        while (!order.empty()) {
-            TreeNode* node = order.top();
-            order.pop();
-
-            int sum = node->val;
-            int count = 1;
-
-            if (node->left != nullptr) {
-                sum += data[node->left].first;
-                count += data[node->left].second;
-            }
-
-            if (node->right != nullptr) {
-                sum += data[node->right].first;
-                count += data[node->right].second;
-            }
-
-            int avg = calculateAverage(sum, count);
-
-            if (avg == node->val) {
-                ans++;
-            }
-
-            data[node] = {sum, count};
-        }
+        solve(root, ans);
 
         return ans;
         
